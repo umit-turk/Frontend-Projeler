6b7ef33e77ea1a74d53a75539ae7bd70fd6cc040
Umit Turk <uyt.turk@gmail.com>
2021-07-11 16:52:55 +0300

    kripto son

---
+++ Kriptopara/kripto/src/components/search.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Coin from "./coin";
+
+function Search() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLocaleLowerCase())
+  );
+
+  return (
+    <div className="coin-app">
+        <h1>Search a currency</h1>
+      <div className="coin-search">
+        <form>
+          <input
+            type="text"
+            placeholder="Search..."
+            onChange={handleChange}
+            className="coin-input"
+          />
+        </form>
+      </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default Search;
