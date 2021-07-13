0bc331b7ea7068a0a49731bcef05194f96794633
Umit Turk <uyt.turk@gmail.com>
2021-07-13 20:51:55 +0300

    edit

--- Notesapp/notesapp/src/components/List.js
+++ Notesapp/notesapp/src/components/List.js
@@ -5,11 +5,31 @@ import { DataContext } from "./DataProvider";
 function List() {
     const [todos, setTodos] = useContext(DataContext);
 
+    const switchComplete = id => {
+     const newTodos = [...todos]
+     newTodos.forEach((todo, index) => {
+       if(index === id){
+         todo.complete = !todo.complete
+       }
+     })
+     setTodos(newTodos)
+    }
+
+    const handleEditTodos = (editvalue, id) => {
+      const newTodos = [...todos]
+      newTodos.forEach((todo, index) => {
+        if(index === id){
+          todo.name = editvalue
+        }
+      })
+      setTodos(newTodos)
+    }
+
   return (
     <ul>
       {
           todos.map((todo, index) => (
-              <ListItem todo={todo} key={index} id={index} />
+              <ListItem todo={todo} key={index} id={index} checkComplete={switchComplete} handleEditTodos={handleEditTodos} />
           ))
       }
     </ul>
