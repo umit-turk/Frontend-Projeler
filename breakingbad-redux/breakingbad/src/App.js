0f32e8b66304cb8cb6a9ec01cd52496935b94b8b
Umit Turk <uyt.turk@gmail.com>
2021-08-11 16:02:53 +0300

    quotes detail

--- breakingbad-redux/breakingbad/src/App.js
+++ breakingbad-redux/breakingbad/src/App.js
@@ -1,14 +1,27 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
+import Quotes from "./pages/Quotes";
+import "./App.css";
 
 function App() {
   return (
     <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Characters</Link>
+          </li>
+          <li>
+            <Link to="/quotes">Quotes</Link>
+          </li>
+        </ul>
+      </nav>
       <div>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/char/:char_id" component={Detail} />
+          <Route path="/quotes" component={Quotes} />
         </Switch>
       </div>
     </Router>
