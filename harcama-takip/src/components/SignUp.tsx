1f852676ae030b28677b95b5bf5a490f2b45cb20
Umit Turk <uyt.turk@gmail.com>
2021-08-13 20:32:14 +0300

    login

--- harcama-takip/src/components/SignUp.tsx
+++ harcama-takip/src/components/SignUp.tsx
@@ -1,10 +1,9 @@
-import { Form, Input,  Button, message,  Space} from "antd";
+import { Form, Input,  Button,  Space} from "antd";
 import { useHistory } from "react-router-dom";
 import api from "../utils/api";
+import showError from "../utils/showError";
 
-const showError = (errorMessage: string) => {
-    message.error(errorMessage);
-  };
+
 
 function SignUp() {
   const layout = {
@@ -28,7 +27,7 @@ function SignUp() {
   const onFinish = async (values: any) => {
       try {
           await api.post("/users/register", values);
-        history.push("/login");
+        history.push("/login", {newSignUp: true});
       } catch(error) {
           console.log({error});
           showError(( error as any).response.data.errorMessage);
