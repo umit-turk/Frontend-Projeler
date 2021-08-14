91c12adf0fe8505195dccb6d5c34b451ef88ccda
Umit Turk <uyt.turk@gmail.com>
2021-08-14 15:49:25 +0300

    edit,delete

--- harcama-takip/src/components/Categories.tsx
+++ harcama-takip/src/components/Categories.tsx
@@ -1,13 +1,19 @@
-import { Button, Input, Table, Tag, Form, Select } from "antd";
+import { Button, Input, Table, Tag, Form, Select, Space } from "antd";
 import Modal from "antd/lib/modal/Modal";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppState } from "../store";
-import { addCategory, getCategories } from "../store/actions/categoryActions";
+import {
+  addCategory,
+  deleteCategory,
+  getCategories,
+  updateCategory,
+} from "../store/actions/categoryActions";
 import { Category, CategoryForm } from "../types/category";
 import { SketchPicker } from "react-color";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 
-type Mode = "new" | "edit";
+type Mode = "new" | "edit" | "delete";
 
 const emptyForm: CategoryForm = {
   name: "",
@@ -23,6 +29,8 @@ function Categories() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [mode, setMode] = useState<Mode>("new");
   const [form, setForm] = useState<CategoryForm>(emptyForm);
+  const [updateId, setUpdateId] = useState<number | null>(null);
+  const [deleteId, setDeleteId] = useState<number | null>(null);
 
   const showModal = (mode: Mode) => {
     setIsModalVisible(true);
@@ -31,15 +39,22 @@ function Categories() {
 
   const handleOk = () => {
     // Mode degerine göre create or action creator fonksiyonunu çağır.
-    dispatch(addCategory(form));
+    if (mode === "new") dispatch(addCategory(form));
+    else if (mode === "edit" && typeof updateId === "number")
+      dispatch(updateCategory(form, updateId));
+    else if (mode === "delete" && typeof deleteId === "number")
+      dispatch(deleteCategory(deleteId));
     setIsModalVisible(false);
     setMode("new");
     setForm(emptyForm);
+    setUpdateId(null);
   };
 
   const handleCancel = () => {
     setIsModalVisible(false);
     setMode("new");
+    setUpdateId(null);
+    setDeleteId(null);
   };
 
   const columns = [
@@ -57,16 +72,29 @@ function Categories() {
       },
     },
 
-    /* {
-          title: 'Action',
-          key: 'action',
-          render: (text, record) => (
-            <Space size="middle">
-              <a>Invite {record.name}</a>
-              <a>Delete</a>
-            </Space>
-          ),
-        }, */
+    {
+      title: "Action",
+      key: "action",
+      render: (text: string, category: Category) => (
+        <Space size="middle">
+          <EditOutlined
+            style={{ color: "blue" }}
+            onClick={() => {
+              showModal("edit");
+              setForm(category);
+              setUpdateId(category.id);
+            }}
+          />
+          <DeleteOutlined
+            style={{ color: "red" }}
+            onClick={() => {
+              showModal("delete");
+              setDeleteId(category.id);
+            }}
+          />
+        </Space>
+      ),
+    },
   ];
 
   const dispatch = useDispatch();
@@ -78,48 +106,56 @@ function Categories() {
   return (
     <>
       <div>
+        <div style={{display: "flex", justifyContent: "flex-end", marginBottom: 10}}>
         <Button type="primary" onClick={() => showModal("new")}>
           New Category
         </Button>
+        </div>
         <Modal
-          title={mode === "new" ? "Create New Category" : "Update Category"}
+          title={
+            mode === "new"
+              ? "Create New Category"
+              : mode === "edit"
+              ? "Update Category"
+              : "Delete Category"
+          }
           visible={isModalVisible}
           onOk={handleOk}
           onCancel={handleCancel}
-          okButtonProps={{disabled: !form.name}}
+          okButtonProps={{ disabled: !(mode === "delete") && !form.name }}
         >
-          <Form
-          labelCol={{span: 8}}
-          wrapperCol={{span: 16}}
-          >
-          <Form.Item
-            label="Category Name"
-          >
-            <Input
-              name="name"
-              value={form.name}
-              onChange={(e) => setForm({ ...form, name: e.target.value })}
-            />
-          </Form.Item>
-          <Form.Item label="Category Type">
-            <Select
-              defaultValue="expense"
-              onChange={(type) => setForm({ ...form, type })}
-            >
-              <Select.Option value="demo">Income</Select.Option>
-              <Select.Option value="expense">Expense</Select.Option>
-            </Select>
-          </Form.Item>
-          <Form.Item label="Color">
-            <SketchPicker
-              color={form.color}
-              onChange={(color) => setForm({ ...form, color: color.hex })}
-            />
-          </Form.Item>
-          </Form>
+          {mode === "edit" || mode === "new" ? (
+            <Form labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+              <Form.Item label="Category Name">
+                <Input
+                  name="name"
+                  value={form.name}
+                  onChange={(e) => setForm({ ...form, name: e.target.value })}
+                />
+              </Form.Item>
+              <Form.Item label="Category Type">
+                <Select
+                  defaultValue="expense"
+                  value={form.type}
+                  onChange={(type) => setForm({ ...form, type })}
+                >
+                  <Select.Option value="demo">Income</Select.Option>
+                  <Select.Option value="expense">Expense</Select.Option>
+                </Select>
+              </Form.Item>
+              <Form.Item label="Color">
+                <SketchPicker
+                  color={form.color}
+                  onChange={(color) => setForm({ ...form, color: color.hex })}
+                />
+              </Form.Item>
+            </Form>
+          ) : mode === "delete" ? (
+            <>Are you sure</>
+          ) : null}
         </Modal>
       </div>
-      <Table columns={columns} dataSource={data} />
+      <Table loading={loading} columns={columns} dataSource={data} />
     </>
   );
 }
